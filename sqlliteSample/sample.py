fc5ea59d23d24775bd5921e3aea5bd4af28fc988
tktktakatuska <blue.mountain.syota@iclouud.com>
2022-11-12 10:39:15 +0900

    test

--- sqlliteSample/sample.py
+++ sqlliteSample/sample.py
@@ -4,28 +4,33 @@ from unicodedata import name
 # TEST.dbを作成する
 # すでに存在していれば、それにアスセスする。
 def dbAccess(dbname):
-    connect = sqlite3.connect(dbname) 
-    return connect
+    conn = sqlite3.connect(dbname) 
+    return conn
 
 #テーブル作成
-def createTable(createSQL):
+def dbCreateTable(createSQL):
     cur.execute(createSQL)
 
 #レコード追加
 def dbInsert(insertSQL):
     cur.execute(insertSQL)
+    cur.execute('INSERT INTO persons(name) values("Hanako")')
+    cur.execute('INSERT INTO persons(name) values("Zirou")')
+    conn.commit()
 
-#セレクト分
+#セレクト実行
 def dbSelect(selectSql):
     cur.execute(selectSql)
+    print(cur.fetchall())
  
 # データベースへコミット。    
 def dbCommit():
-    conn.commit
+    conn.commit()
  
      
 # データベースへのコネクションを閉じる。(必須)    
 def dbClose():
+    cur.close()
     conn.close()
     
 
@@ -34,12 +39,34 @@ if __name__ == '__main__':
     #dbアクセス
     dbname    = 'TEST.db'
     conn      = dbAccess(dbname)
-    createSQL = 'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT,name STRING)'
+    field     = 'name'
+    table     =  'persons'
+    
+    createSQL =  'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT,name STRING)'
+    insertSQL = 'INSERT INTO persons(name) values("Taro")'
+    selectSQL = 'SELECT * FROM persons'
     
     # sqliteを操作するカーソルオブジェクトを作成
     cur     = conn.cursor()
     connect = sqlite3.connect(dbname) 
-    createTable(createSQL)
+    
+    #dbCreateTable(createSQL)
+    
+    #レコード追加
+    dbInsert(insertSQL)
+    
+    #セレクト文出力
+    dbSelect(selectSQL)
+
+    
+    #コミット
+    dbCommit()
+    
+    #クローズ
+    dbClose()
+    
+    
+    
     
     
 

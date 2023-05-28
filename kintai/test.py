4cfaec123df3139ef4a09f23805274a3cf6c189a
tktktakatuska <blue.mountain.syota@icloud.com>
2023-05-28 23:48:19 +0900

    勤怠自動入力バッチ作成

--- kintai/test.py
+++ kintai/test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 options = Options()
 options.add_argument("--user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data")
@@ -11,67 +13,69 @@ driver = webdriver.Chrome(ChromeDriverManager().install() , options=options)
 driver.get("http://redmine.atue.jp/projects/atue-company-overtime/issues/new")
 time.sleep(1)
 
-# タイトルに値をセット
-title = driver.find_element('id' , 'issue_subject')
-time.sleep(1)
-title.send_keys('5/25 残業申請(社内)')
+def sendKeyById(idName , sendString):
+    title = driver.find_element('id' , idName)
+    time.sleep(1)
+    title.clear()
+    title.send_keys(sendString)
 
-# タイトルに値をセット
-description = driver.find_element('id' , 'issue_description')
-time.sleep(1)
-description.send_keys('勤務時間 9:30～18:30(休憩1H)\n残業時間 18:30～19:30(1H)\nオッズパーク案件 投票内容入力画面単体試験実施の為')
+def selectVlueById(idName , selectValue):
+    WebElement = driver.find_element('id' , idName)
+    select = Select(WebElement)
+    time.sleep(1)
+    select.select_by_visible_text(selectValue)
+    
+def checkBoxClickById(idName ):
+    title = driver.find_element('id' , idName)
+    time.sleep(1)
+    title.click()
+    
+def checkBoxClickByXpath(idName ):
+    title = driver.find_element(By.XPATH , idName)
+    time.sleep(1)
+    title.click()
 
-# statusに値をセット
-status = driver.find_element('id' , 'issue_status_id')
-time.sleep(1)
-status.send_keys('申請中')
 
-# # 担当者に値をセット
-# tantousya = driver.find_element('id' , 'issue_assigned_to_id')
-# time.sleep(1)
-# tantousya.send_keys('高塚祥太')
-
+    
 # # 作業場所に値をセット
-# basyo = driver.find_element('id' , 'issue_custom_field_values_6')
-# time.sleep(1)
-# basyo.send_keys('池袋')
-
+sendKeyById('issue_custom_field_values_6' , '池袋') 
 # # 就業時刻に値をセット
-# workTime = driver.find_element('id' , 'issue_custom_field_values_6')
-# time.sleep(1)
-# workTime.send_keys('18:30~19:30')
-
+sendKeyById('issue_custom_field_values_8' , '18:30~19:30') 
 # # 開始日に値をセット
-# startDate = driver.find_element('id' , 'issue_start_date')
-# time.sleep(1)
-# startDate.send_keys('2023-05-28')
 
-# 期日に値をセット
-dueDate = driver.find_element('id' , 'issue_due_date')
-time.sleep(1)
-dueDate.send_keys('2023-05-29')
+sendKeyById('issue_start_date' , '2023-05-28') 
+# # 期日に値をセット
+sendKeyById('issue_due_date' , '2023-05-29') 
+# # 予定工数
+sendKeyById('issue_estimated_hours' , '1.0') 
+#工数実績
+sendKeyById('issue_custom_field_values_12' , '1.1') 
+# タイトルに値をセット
+sendKeyById('issue_subject' , '5/25 残業申請(社内)') 
+# 説明文に値をセット
+sendKeyById('issue_description' , '勤務時間 9:30～18:30(休憩1H)\n残業時間 18:30～19:30(1H)\nオッズパーク案件 投票内容入力画面単体試験実施の為') 
 
-# 予定工数
-dueDate = driver.find_element('id' , 'issue_estimated_hours')
-time.sleep(1)
-dueDate.send_keys('1.0')
+# # 許可者
+selectVlueById('issue_custom_field_values_13' , '弘道 相原')
+# # 作業区分
+selectVlueById('issue_custom_field_values_14' , 'その他')
+# セレクトボックスの指定
+selectVlueById('issue_assigned_to_id' , '高塚 祥太')
 
-# 実績校数
-zisseki = driver.find_element('id' , 'issue_estimated_hours')
-time.sleep(1)
-zisseki.send_keys('1.1')
+#checkBoxのクリック
+checkBoxClickById('issue_watcher_user_ids_69')
+checkBoxClickById('issue_watcher_user_ids_101')
+checkBoxClickById('issue_watcher_user_ids_85')
+checkBoxClickById('issue_watcher_user_ids_6')
+checkBoxClickById('issue_watcher_user_ids_100')
+checkBoxClickById('issue_watcher_user_ids_99')
+checkBoxClickById('issue_watcher_user_ids_102')
+checkBoxClickById('issue_watcher_user_ids_98')
 
+#status
+selectVlueById('issue_status_id' , '申請中')
 
-# 許可者
-kyoka = driver.find_element('id' , 'issue_custom_field_values_13')
-time.sleep(1)
-kyoka.send_keys('弘道 相原')
-
-
-# 作業区分
-kubun = driver.find_element('id' , 'issue_custom_field_values_14')
-time.sleep(1)
-kubun.send_keys('その他')
+checkBoxClickByXpath('//*[@id="issue-form"]/input[1]')
 
 time.sleep(100000)
 
